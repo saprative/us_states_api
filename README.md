16d88a5bd591589db6bb98a2cf269d49c39eeb4c
saprative <saprative@gmail.com>
2017-06-12 09:01:15 +0530

    Final

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 States List App
 ===============
+States is standalone app that can be intregrated with any existing flask app
+just by importing and adding the states blueprint and is ready to be pushed into
+production. States shows the list states in USA in a alphbetical order. 
 
 # Install
 * Install python3
 * pip install -r requirments.txt
+* start : python manage.py
+* push into production by importing the states blueprint and intregating it with
+  any flask app
  
 
 # Data
--- states/__init__.py
+++ states/__init__.py
@@ -10,7 +10,9 @@ from flask_restplus import (
         Api, 
         reqparse
 )
-import math
+import math,os
+dir = os.path.dirname(__file__)
+state_file = os.path.join(dir,'states.json')
 
 states = Blueprint('states',__name__)
 api = Api(states)
@@ -18,7 +20,8 @@ api = Api(states)
 pagination_arguments = reqparse.RequestParser()
 pagination_arguments.add_argument('page', type=int, default=1)
 
-with open('states/states.json') as data_file:
+
+with open(state_file) as data_file:
     data = json.load(data_file)
 
 @api.route('/states')
